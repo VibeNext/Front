dbe0a5a85fe81fd9067fc7d2e8c8793c76a80c3b
SeongJoo Kim <2371076@ewhain.net>
2025-10-31 11:10:11 +0900

    Update src/styles/theme.js
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,13 @@
-// prettier-ignore
-const globalCss = {
-  'body': {
-    '@supports (height: 100svh)': { height: '100svh' },
-    '@supports not (height: 100svh)': { height: '100vh' },
-
-    '@media (max-width: 480px)': {
-      '@supports (width: 100svw)': { width: '100svw' },
-      '@supports not (width: 100svw)': { width: '100vw' },
-    },
-    '@media (min-width: 480px)': {
-      width: '480px',
-      margin: '0 auto',
-    },
-  },
+const colors = {
+  primary: '#3498db',
+  gray: '#bdc3c7',
 };
+
+const fonts = {};
 
 const theme = {
-  globalCss,
+  colors,
+  fonts,
 };
+
 export { theme };
