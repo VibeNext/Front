008fc2c624f1719e973b40c7841b85e311eae8aa
SeongJoo Kim <2371076@ewhain.net>
2025-11-01 19:13:19 +0900

    Update src/index.jsx
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
-import GlobalStyle from './styles/glabal';
+import GlobalStyle from './styles/global';
 import Router from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
