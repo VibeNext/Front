3bf2fb673c36d568924f1466dbf82e0fe556b729
lloviazuu <sungjoo3321@gmail.com>
2025-11-05 21:29:49 +0900

    💄 TopNavigation 컴포넌트

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,10 +1,33 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import TopNavigation from './components/common/TopNavigation.jsx';
+import TopNavigationBeforeLogin from './components/common/TopnNavigationBeforeLogin';
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>홈</div>} />
+        <Route
+          path='/'
+          element={
+            <div>
+              <TopNavigationBeforeLogin />
+              <TopNavigation />
+
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+              <h>kk</h>
+
+              <h>kk</h>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
