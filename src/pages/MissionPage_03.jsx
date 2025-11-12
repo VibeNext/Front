592ded975749b577c1d7ad27ce8c16e09efa50d5
Yeonseo Choi <juwelys@ewhain.net>
2025-11-12 19:51:37 +0900

    MissionPage 문제설명(step03 그림 제외 완성) (#39)

--- src/pages/MissionPage_03.jsx
+++ src/pages/MissionPage_03.jsx
@@ -7,15 +7,66 @@ import AnswerCheckContainer from '../components/common/AnswerCheckContainer/Answ
 import MissionDescription from '../components/common/MissionDescription.jsx';
 import MissionHeader from '../components/common/MissionHeader.jsx';
 import TopNavigation from '../components/common/TopNavigation.jsx';
-import missionData from '../data/missionData.js';
+
+import robotTaleImg from '../assets/icons/robot_tail.png';
+import trashTaleImg from '../assets/icons/trash_tail.png';
+//import taleImg from '../assets/icons/orinigal_tail.png';
 
 const MissionPage_03 = () => {
   const { missionId } = useParams();
-  const step = 3; 
   const mission = Number(missionId);
 
-  // 데이터 확인
-  const missionInfo = missionData?.[step]?.[mission];
+  const renderMissionContent = () => {
+    switch (mission) {
+      case 1:
+        return (
+          <>
+            <p>똑똑한 당신은 로봇 개발자! 오늘은 로봇 청소기를 개발하고 있어요.</p>
+            <p style={{ marginBottom: '1rem' }}>아래 그림을 보고, 로봇이 모든 쓰레기를 청소할 수 있도록 명령을 입력해보세요.</p>
+            <p style={{color: '#868BA3'}}>* 이 로봇 청소기는 한 번에 한 가지 일만 할 수 있어요.</p>
+            <p style={{color: '#868BA3'}}>** '앞으로 한 칸 이동'하거나 ‘현재 위치한 한 칸만 청소’할 수 있어요.</p>
+            <ImageRow>
+              <ImageItem>
+                <img src={robotTaleImg} alt="로봇 청소기" />
+              </ImageItem>
+
+              {[...Array(5)].map((_, idx) => (
+                <ImageItem key={idx}>
+                  <img src={trashTaleImg} alt={`쓰레기 ${idx + 1}`} />
+                </ImageItem>
+              ))}
+            </ImageRow>
+          </>
+        );
+
+      case 2:
+        return (
+          <>
+            <p>앗, 로봇 청소기가 이상하게 작동하고 있어요!</p>
+            <p style={{ marginBottom: '1rem' }}>로봇 청소기 반복문이 잘못된 이유를 설명하고, 모든 쓰레기를 청소하도록 새로운 반복문을 작성해봅시다!</p>
+            <p style={{color: '#868BA3'}}>* 이 로봇 청소기는 한 번에 한 가지 일만 할 수 있어요.</p>
+            <p style={{color: '#868BA3'}}>** ‘앞으로 한 칸 이동’하거나 ‘회전(왼쪽/오른쪽 90도)’할 수 있어요.</p>
+            
+          </>
+        );
+
+      case 3:
+        return (
+          <>
+            <p>이번에는 지그재그로 놓인 쓰레기를 모두 청소해야 해요!</p>
+            <p style={{ marginBottom: '1rem' }}>아래 그림을 보고, 모든 쓰레기를 깨끗이 청소하도록 새로운 반복문을 직접 작성해봅시다!</p>
+            <p style={{color: '#868BA3'}}>* 이 로봇 청소기는 한 번에 한 가지 일만 할 수 있어요.</p>
+            <p style={{color: '#868BA3'}}>** ‘앞으로 한 칸 이동’하거나 ‘회전(왼쪽/오른쪽 90도)’할 수 있어요.</p>
+            
+            
+          </>
+        );
+
+      default:
+        return <p>미션 설명을 불러올 수 없습니다.</p>;
+    }
+  };
+
 
   return (
     <Wrapper>
@@ -27,7 +78,11 @@ const MissionPage_03 = () => {
         <MissionHeader
           stepId={3}
           stepNumber="03 반복"
-          title={missionInfo.title}
+          title={mission === 1
+            ? '개발자의 로봇청소기: 직선'
+            : mission === 2
+            ? '개발자의 로봇청소기: 2x2'
+            : '개발자의 로봇청소기: 지그재그'}
           initialStep={mission}
         />
 
@@ -35,10 +90,9 @@ const MissionPage_03 = () => {
         <MainLayout>
           {/* 왼쪽: 문제 설명 + 정답 확인 */}
           <LeftPanel>
-            <MissionDescription
-              instruction={missionInfo.instruction}
-              images={missionInfo.images}
-            />
+            <MissionDescription>
+              {renderMissionContent()}
+            </MissionDescription>
             <AnswerCheckContainer status="default">
             </AnswerCheckContainer>
           </LeftPanel>
@@ -92,5 +146,28 @@ const RightPanel = styled.div`
   flex-direction: column;
 `;
 
+
+//이미지 
+const ImageRow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 0.06188rem;
+  margin-top: 2.64rem;
+`;
+
+const ImageItem = styled.div`
+  width: 6.07613rem;
+  height: 6.02044rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin:0;
+
+  img {
+    width: 80%;
+    height: auto;
+  }
+`;
 
 
