5cae5e8dfa3f1dae7b1cfe5b68a466f7d25fc0ca
lloviazuu <sungjoo3321@gmail.com>
2025-11-18 03:18:00 +0900

    💄Topnavigation수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,34 +1,38 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
 import MissionPage_01 from './pages/MissionPage_01.jsx';
 import MissionPage_02 from './pages/MissionPage_02.jsx';
 import MissionPage_03 from './pages/MissionPage_03.jsx';
+import MyPage from './pages/MyPage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/mypage' element={<MyPage />} />
 
         {/* Step 01 (순차) */}
         <Route
-          path="/step/1/mission/:missionId"
+          path='/step/1/mission/:missionId'
           element={<MissionPage_01 key={window.location.pathname} />}
         />
 
-
         {/* Step 02 (조건) */}
         <Route
-          path="/step/2/mission/:missionId"
+          path='/step/2/mission/:missionId'
           element={<MissionPage_02 key={window.location.pathname} />}
         />
 
         {/* Step 03 (반복) */}
         <Route
-          path="/step/3/mission/:missionId"
+          path='/step/3/mission/:missionId'
           element={<MissionPage_03 key={window.location.pathname} />}
         />
-
       </Routes>
     </BrowserRouter>
   );
